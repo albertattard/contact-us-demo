8c34fbfae8a7fd5508aa36444a31835ef62a0319
Andres Mulin <andres.mulin@thoughtworks.com>
2020-08-10 14:12:09 +0200

    Additional Tests added

--- src/main/java/demo/boot/office/OfficeController.java
+++ src/main/java/demo/boot/office/OfficeController.java
@@ -1,24 +1,25 @@
 package demo.boot.office;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
 @AllArgsConstructor
+@RestController
 public class OfficeController {
 
   private final ContactUsService service;
 
-  @GetMapping( "/offices" )
+  @RequestMapping( "/offices" )
   public List<Office> offices() {
     return service.list();
   }
 
-  //  @GetMapping( "/offices/{country}" )
-  //  public List<Office> officesIn( final @PathVariable( "country" ) String country ) {
-  //    return service.findAllInCountry( country );
-  //  }
+  @RequestMapping( "/offices/{country}" )
+  public List<Office> officesIn( final @PathVariable( "country" ) String country ) {
+    return service.findAllInCountry( country );
+  }
 }
